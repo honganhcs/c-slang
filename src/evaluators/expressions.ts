1a0be7eec519a403d1beafbcb30918b58e5ac675
Van-Nhi Ba Nguyen <nbvannhi@gmail.com>
2023-03-11 00:35:10 +0800

    feat: handle assignments

--- src/evaluators/expressions.ts
+++ src/evaluators/expressions.ts
@@ -1,3 +1,5 @@
+import { AssignmentOperator } from 'estree'
+import { getGlobalFrame, updateFrame } from '../createContext'
 import { actualValue } from '../interpreter/interpreter'
 
 export function* evaluateConditionalExpression(
@@ -7,4 +9,33 @@ export function* evaluateConditionalExpression(
   context: any
 ) {
   return test ? yield* actualValue(consequent, context) : yield* actualValue(alternate, context)
+}
+
+const assignmentMicrocode = {
+  '=': (l: any, r: any) => r,
+  '+=': (l: any, r: any) => l + r,
+  '-=': (l: any, r: any) => l - r,
+  '*=': (l: any, r: any) => l * r,
+  '/=': (l: any, r: any) => l / r,
+  '%=': (l: any, r: any) => l % r,
+}
+
+export function* evaluateAssignmentExpression(
+  operator: AssignmentOperator,
+  name: any,
+  left: any,
+  right: any,
+  context: any
+) {
+  console.log("in eval assingmnet")
+  console.log(name)
+  console.log(left)
+  console.log(right)
+  const frame = getGlobalFrame(context)
+  const id = frame[name]
+  const value = assignmentMicrocode[operator](left, right)
+  console.log(value)
+  updateFrame(frame, name, id.kind, value)
+  console.log(context)
+  return value
 }
